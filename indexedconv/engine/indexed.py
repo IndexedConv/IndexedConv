fix memory computation

indexedconv/engine/indexed.py
@@ -133,7 +133,7 @@ class IndexedConv(nn.Module):
         super(IndexedConv, self).__init__()
         self.logger = logging.getLogger(__name__ + '.IndexedConv')
 
-        print('!!! bmm + no_grad version !!!')
+        self.logger.info('!!! bmm + no_grad version !!!')
         groups = 1
 
         kernel_size = indices.shape[0]